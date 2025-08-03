1089e1f2ebc49607c56a7201fa54805c662d6cd0
Rodrigo Prado <rgp@cesar.org.br>
2025-08-03 15:35:19 -0300

    otimizando conftest.py e fixture

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    """
+    Fixture que inicializa e finaliza o WebDriver para cada teste.
+    """
+    # instance = webdriver.Chrome()
+    instance = webdriver.Firefox()
+
+    yield instance
+
+    instance.quit()
