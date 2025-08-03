fdfe00fbe5b7d239ffb88313eb8b23dc8f89bdec
Rodrigo Prado <rgp@cesar.org.br>
2025-08-03 14:21:03 -0300

    first commit

---
+++ test_home_page.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+
+def test_verify_page_title():
+    """
+    Este teste abre o site demoqa.com e verifica o título da página.
+    """
+    # Inicializa o driver do Chrome
+    # driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
+
+    # Abre a URL
+    driver.get("https://demoqa.com")
+
+    # Pega o título da página
+    page_title = driver.title
+
+    # Verifica se o título é o esperado
+    assert page_title == "DEMOQA"
+
+    # Fecha o navegador
+    driver.quit()
---
+++ teste.py
@@ -0,0 +1,6 @@
+import cowsay
+import selenium
+
+print("Selenium version:", selenium.__version__)
+cowsay.cow("Good Mooooorning!")
+
