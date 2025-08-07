efb0fffdcefa37f7eaf9c91b11affe0d4cbe64c7
Rodrigo Prado <rgp@cesar.org.br>
2025-08-07 00:20:52 -0300

    adding new tests

--- Dia4/test_buttons.py
+++ Dia4/test_buttons.py
@@ -9,18 +9,18 @@ def test_button_clicks(driver):
     driver.get("https://demoqa.com/buttons")
     actions = ActionChains(driver)
 
-    # # 1. Double Click
-    # double_click_btn = driver.find_element(By.ID, "doubleClickBtn")
-    # actions.double_click(double_click_btn).perform()
-    # double_click_message = driver.find_element(By.ID, "doubleClickMessage")
-    # assert "You have done a double click" in double_click_message.text
+    # 1. Double Click
+    double_click_btn = driver.find_element(By.ID, "doubleClickBtn")
+    actions.double_click(double_click_btn).perform()
+    double_click_message = driver.find_element(By.ID, "doubleClickMessage")
+    assert "You have done a double click" in double_click_message.text
     
 
-    # # 2. Right Click
-    # right_click_btn = driver.find_element(By.ID, "rightClickBtn")
-    # actions.context_click(right_click_btn).perform()
-    # right_click_message = driver.find_element(By.ID, "rightClickMessage")
-    # assert "You have done a right click" in right_click_message.text
+    # 2. Right Click
+    right_click_btn = driver.find_element(By.ID, "rightClickBtn")
+    actions.context_click(right_click_btn).perform()
+    right_click_message = driver.find_element(By.ID, "rightClickMessage")
+    assert "You have done a right click" in right_click_message.text
     
     # 3. Dynamic Click (Simples)
     # O XPath aqui é mais complexo porque o botão não tem um ID fixo
